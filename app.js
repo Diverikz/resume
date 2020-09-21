bc386878d0af5e56775c52f8304bf3785e969ea3
Your Name <you@example.com>
2020-09-21 21:32:59 +0300

    da da ya

---
+++ app.js
@@ -0,0 +1,18 @@
+$(function() {
+
+    let header = $("#header");
+    let intro = $("#intro");
+    let IntroH = intro.innerHeight();
+    let scrollPos = $(window).scrollTop();
+
+    $(window).on("scroll  resize", function() {
+        scrollPos = $(this).scrollTop();
+
+    if( scrollPos > IntroH) {
+        header.addClass("fixed");
+    } else {
+        header.removeClass("fixed");
+    }
+    });
+
+});
