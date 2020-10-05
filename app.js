521f2bc9d51fe38daf9e0d0e5c7c806a5f700915
Your Name <you@example.com>
2020-10-05 11:07:56 +0300

    format

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
-$(function() {
+$(function () {
+  let header = $("#header");
+  let intro = $("#intro");
+  let IntroH = intro.innerHeight();
+  let scrollPos = $(window).scrollTop();
 
-    let header = $("#header");
-    let intro = $("#intro");
-    let IntroH = intro.innerHeight();
-    let scrollPos = $(window).scrollTop();
+  $(window).on("scroll  resize", function () {
+    scrollPos = $(this).scrollTop();
 
-    $(window).on("scroll  resize", function() {
-        scrollPos = $(this).scrollTop();
-
-    if( scrollPos > IntroH) {
-        header.addClass("fixed");
+    if (scrollPos > IntroH) {
+      header.addClass("fixed");
     } else {
-        header.removeClass("fixed");
+      header.removeClass("fixed");
     }
-    });
-
+  });
 });
